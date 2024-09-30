afe9b6706d3474a2c4d50341c0f9b0962eb54bf9
João Victor <86979717+joaorange@users.noreply.github.com>
2024-09-30 15:54:56 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -65,17 +65,17 @@ def plot_data(grouped_data, selected_variable):
     # Anotando os pontos com ajuste de posição
     for index, row in grouped_data.iterrows():
         plt.text(row['data'], row[selected_variable] + (index % 2) * 0.5, f'{row[selected_variable]:.2f}',
-                 horizontalalignment='left', size=24, color='black', weight='semibold')
+                 horizontalalignment='left', size=32, color='black', weight='semibold')
 
-    plt.title(f'{selected_variable} de Perfume (extratos) Importado para São Paulo', fontsize=40, weight='bold')
-    plt.xlabel('Data', fontsize=34)
-    plt.ylabel(selected_variable, fontsize=34)
+    plt.title(f'{selected_variable} de Perfume (extratos) Importado para São Paulo', fontsize=50, weight='bold')
+    plt.xlabel('Data', fontsize=40)
+    plt.ylabel(selected_variable, fontsize=40)
     plt.xticks(rotation=45)
-    plt.xticks(fontsize=30, rotation=45)
-    plt.yticks(fontsize=30)
+    plt.xticks(fontsize=35, rotation=45)
+    plt.yticks(fontsize=35)
     plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
-    plt.grid(color='grey', linestyle='--', linewidth=0.7, alpha=0.7)
+    plt.grid(color='grey', linestyle='--', linewidth=1.5, alpha=0.7)
     plt.tight_layout()
 
     # Exibindo o gráfico
